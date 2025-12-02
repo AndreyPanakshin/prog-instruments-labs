1c8eb7bf43c24e500966403cc414a987ca310dc5
AndreyPanakshin <164353054+AndreyPanakshin@users.noreply.github.com>
2025-12-02 17:42:27 +0400

    Add func that finds invalid rows

--- main.py
+++ main.py
@@ -13,4 +13,13 @@ def validate_row(row: list[str], patterns: list[re.Pattern]) -> bool:
     """Checks one string for compliance with all patterns."""
     if len(row) != len(patterns):
         return False
+
     return all(pattern.fullmatch(value.strip()) for pattern, value in zip(patterns, row))
+
+def find_invalid_rows(data: list[list[str]], patterns: list[re.Pattern]) -> list[int]:
+    """Returns the number with invalid data."""
+    invalid_lines = []
+    for index, row in enumerate(data[1:], start=0):  # пропускаем заголовок
+        if not validate_row(row, patterns):
+            invalid_lines.append(index)
+    return invalid_lines
