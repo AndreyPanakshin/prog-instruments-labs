db3880d3e788c0adfdbc7538a6e3405fff0e3bd9
AndreyPanakshin <164353054+AndreyPanakshin@users.noreply.github.com>
2025-12-05 15:31:00 +0400

    Adding documentation and type annotations

--- calculator.py
+++ calculator.py
@@ -1,29 +1,34 @@
+from typing import Union
+
+Number = Union[int, float]
+
+
 class Calculator:
     
     @staticmethod
-    def add(a, b):
+    def add(a: Number, b: Number) -> Number:
         return a + b
     
     @staticmethod
-    def subtract(a, b):
+    def subtract(a: Number, b: Number) -> Number:
         return a - b
     
     @staticmethod
-    def multiply(a, b):
+    def multiply(a: Number, b: Number) -> Number:
         return a * b
     
     @staticmethod
-    def divide(a, b):
+    def divide(a: Number, b: Number) -> float:
         if b == 0:
             raise ValueError("Cannot divide by zero!")
         return a / b
     
     @staticmethod
-    def power(a, b):
+    def power(a: Number, b: Number) -> Number:
         return a ** b
     
     @staticmethod
-    def sqrt(a):
+    def sqrt(a: Number) -> float:
         if a < 0:
             raise ValueError("Cannot calculate square root of negative number!")
         return a ** 0.5
